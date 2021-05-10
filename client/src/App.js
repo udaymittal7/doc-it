64be6a752812cd922a5d2f5021853923764092fa
udaymittal7 <mittaluday435@gmail.com>
2021-05-10 15:42:55 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TextEditor from './components/textEditor/TextEditor';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to={`/documents/${uuidv4()}`} />
+        </Route>
+        <Route path='/documents/:id'>
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
